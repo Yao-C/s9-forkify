55706d7471a6041a4ea6401670321eed384527d5
YAO CHENG <yao.cheng@norskluftambulanse.no>
2019-03-06 10:47:44 +0100

    1-2: Search Controller

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,40 @@
 
 import Search from './models/Search'; 
 
-const search = new Search('pizza');
-console.log(search);
-search.getResults();  
+/*******************
+ *** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+
+ const state = {};
+
+
+ /********************
+  * Search Controller
+  */
+
+const controlSearch = async() => {
+  // 1) Get query from view
+  const query = 'pizza'; // To do
+
+  if(query) {
+    // 2) New search object and add to the state
+    state.search = new Search(query);
+
+    // 3) Prepare UI for results
+
+    // 4) Seach for recipes
+    await state.search.getResults();
+
+    // 5) Render result on UI
+    console.log(state.search.result);
+  }
+}
+
+document.querySelector('.search').addEventListener('submit', e=>{
+  e.preventDefault();
+  controlSearch();
+})
