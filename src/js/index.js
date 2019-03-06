428ce10584e05716bd864713eb090706aa4d452c
YAO CHENG <yao.cheng@norskluftambulanse.no>
2019-03-06 11:30:08 +0100

    1-3: Search View - part 1

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,9 @@
 
 // import { add, multiply, ID } from './views/searchView';   // import named export, multiple things
 
+import {elements} from './views/base';
 import Search from './models/Search'; 
+import * as searchView from './views/searchView';
 
 /*******************
  *** Global state of the app
@@ -21,23 +23,27 @@ import Search from './models/Search';
 
 const controlSearch = async() => {
   // 1) Get query from view
-  const query = 'pizza'; // To do
+  // const query = 'pizza'; // To do
+  const query = searchView.getInput();
 
   if(query) {
     // 2) New search object and add to the state
     state.search = new Search(query);
 
     // 3) Prepare UI for results
+    searchView.clearInput();
+    searchView.clearResults();
 
     // 4) Seach for recipes
     await state.search.getResults();
 
     // 5) Render result on UI
-    console.log(state.search.result);
+    // console.log(state.search.result);
+    searchView.renderResults(state.search.result);
   }
 }
 
-document.querySelector('.search').addEventListener('submit', e=>{
+elements.searchForm.addEventListener('submit', e=>{
   e.preventDefault();
   controlSearch();
 })
