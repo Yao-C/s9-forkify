1907312ccceedba2e3478c3a2cb9ef9ff4c9babb
YAO CHENG <yao.cheng@norskluftambulanse.no>
2019-03-06 10:15:24 +0100

    1: Building the Search Model

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,9 @@
 // Global app controller
 
-// import str from './models/Search';   // import default export
 // import { add, multiply, ID } from './views/searchView';   // import named export, multiple things
 
-import axios from 'axios';
+import Search from './models/Search'; 
 
-async function getResults(query) {
-  const proxy = 'https://cors-anywhere.herokuapp.com/';
-  const searchAPI = 'https://www.food2fork.com/api/search/'
-  const key = 'c492ef2abfc2db30b5530e66670307f4';
-  
-  try {
-    const res = await axios(`${proxy}${searchAPI}?key=${key}&query=${query}`);
-    const recipes = res.data.recipes;
-    console.log(recipes);
-  } catch(error) {
-    alert(error);
-  }
-};
-
-getResults('tomato');
+const search = new Search('pizza');
+console.log(search);
+search.getResults();  
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,21 @@
-export default 'I am an exported strings.';
+import axios from 'axios';
+
+export default class Search {
+  constructor(query) {
+    this.query = query;
+  };
+  async getResults() {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const searchAPI = 'https://www.food2fork.com/api/search/'
+    const key = 'c492ef2abfc2db30b5530e66670307f4';
+    
+    try {
+      const res = await axios(`${proxy}${searchAPI}?key=${key}&query=${this.query}`);
+      this.result = res.data.recipes;
+      console.log(this.result);
+    } catch(error) {
+      alert(error);
+    }
+  }
+}
+
